d9a662d0e878eee7d7a60635e68bc1d69c4207ea
Bhumi2021 <abpatel20817@gmail.com>
2021-06-02 17:58:31 +0100

    cucumber project is completed

---
+++ target/Destination/report.js
@@ -0,0 +1,457 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/features/addComment.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Add new comment",
+  "description": "",
+  "id": "add-new-comment",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@all"
+    }
+  ]
+});
+formatter.before({
+  "duration": 8310995300,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "User should able to add new comment successfully",
+  "description": "",
+  "id": "add-new-comment;user-should-able-to-add-new-comment-successfully",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@test2"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "user is on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "user click on New online store is open",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "add title and comment",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "click on new comment",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "user should able to add new comment successfully",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyStepsDefs.user_is_on_homepage()"
+});
+formatter.result({
+  "duration": 112195400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_click_on_New_online_store_is_open()"
+});
+formatter.result({
+  "duration": 1127423600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.add_title_and_comment()"
+});
+formatter.result({
+  "duration": 7155665700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.click_on_new_comment()"
+});
+formatter.result({
+  "duration": 18500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_should_able_to_add_new_comment_successfully()"
+});
+formatter.result({
+  "duration": 34412100,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 1327600100,
+  "status": "passed"
+});
+formatter.uri("src/test/resources/features/buyBookProduct.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Buy product successfully",
+  "description": "",
+  "id": "buy-product-successfully",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@all"
+    }
+  ]
+});
+formatter.before({
+  "duration": 6373821000,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Register user should able to buy books product successfully",
+  "description": "",
+  "id": "buy-product-successfully;register-user-should-able-to-buy-books-product-successfully",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@test4"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "user is on register page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "user enters all required registration details",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user click on register",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "user click on continue",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "user click on books",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "user select First Prize Pies book product",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "user click on add to cart button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "user click on shopping cart",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "user click on checkout",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "User enter billing details and click continue",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "User select shipping option and click continue",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 16,
+  "name": "User select credit card and click continue",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 17,
+  "name": "user enter all visa card details and click continue",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "User click on confirmation",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "user should able to buy book product successfully",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyStepsDefs.user_is_on_register_page()"
+});
+formatter.result({
+  "duration": 1372529900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_enters_all_required_registration_details()"
+});
+formatter.result({
+  "duration": 8005410500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_click_on_register()"
+});
+formatter.result({
+  "duration": 23500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_click_on_continue()"
+});
+formatter.result({
+  "duration": 37900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_click_on_books()"
+});
+formatter.result({
+  "duration": 1114919100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_select_First_Prize_Pies_book_product()"
+});
+formatter.result({
+  "duration": 1599746100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_click_on_add_to_cart_button()"
+});
+formatter.result({
+  "duration": 1697276100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_click_on_shopping_cart()"
+});
+formatter.result({
+  "duration": 23600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_click_on_checkout()"
+});
+formatter.result({
+  "duration": 1823278100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_enter_billing_details_and_click_continue()"
+});
+formatter.result({
+  "duration": 733549300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_select_shipping_option_and_click_continue()"
+});
+formatter.result({
+  "duration": 584902900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_select_credit_card_and_click_continue()"
+});
+formatter.result({
+  "duration": 1202874800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_enter_all_visa_card_details_and_click_continue()"
+});
+formatter.result({
+  "duration": 1643848700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_click_on_confirmation()"
+});
+formatter.result({
+  "duration": 298533100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_should_able_to_buy_book_product_successfully()"
+});
+formatter.result({
+  "duration": 706935800,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 1296390600,
+  "status": "passed"
+});
+formatter.uri("src/test/resources/features/changeCurrency.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Change currency",
+  "description": "",
+  "id": "change-currency",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@all"
+    }
+  ]
+});
+formatter.before({
+  "duration": 6366638700,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "User should able to change currency for all products successfully",
+  "description": "",
+  "id": "change-currency;user-should-able-to-change-currency-for-all-products-successfully",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@test3"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "user is on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "user select currency",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user should able to change currency for all products successfully",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyStepsDefs.user_is_on_homepage()"
+});
+formatter.result({
+  "duration": 611400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_select_currency()"
+});
+formatter.result({
+  "duration": 1357997400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_should_able_to_change_currency_for_all_products_successfully()"
+});
+formatter.result({
+  "duration": 122903300,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 1406237700,
+  "status": "passed"
+});
+formatter.uri("src/test/resources/features/registration.feature");
+formatter.feature({
+  "line": 2,
+  "name": "registration",
+  "description": "",
+  "id": "registration",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@all"
+    }
+  ]
+});
+formatter.before({
+  "duration": 6305906000,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "User should able to register successfully",
+  "description": "",
+  "id": "registration;user-should-able-to-register-successfully",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@test1"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "user is on register page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "user enters all required registration details",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user should able to register successfully",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyStepsDefs.user_is_on_register_page()"
+});
+formatter.result({
+  "duration": 548695300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_enters_all_required_registration_details()"
+});
+formatter.result({
+  "duration": 8006486000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyStepsDefs.user_should_able_to_register_successfully()"
+});
+formatter.result({
+  "duration": 554524300,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 1300615600,
+  "status": "passed"
+});
+});
